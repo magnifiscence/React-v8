40fe8bea99582cf658bba7e3f63602d5a3d1a817
tata <tdivine284@gmail.com>
2024-08-24 02:51:20 +0100

    react

---
+++ adopt_me/src/App.jsx
@@ -0,0 +1,25 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+// delete the Pet component
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <h1>Adopt Me!</h1>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+     </Routes>
+   </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
